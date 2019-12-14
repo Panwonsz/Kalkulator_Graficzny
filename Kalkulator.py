0f413ab9ae76000f7262e250b80a4e672ffc3bca
Panwonsz <kaszewskibartek@gmail.com>
2019-12-14 15:16:52 +0100

    Interfejs

--- Kalkulator.py
+++ Kalkulator.py
@@ -3,105 +3,72 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMe
 
 class WidgetMaker:
     @staticmethod
-    def create_button(text, bind_function = None):
+    def create_button(text, bind_function=None):
         button = QPushButton(text)
+        if bind_function:
+            button.clicked.connect(bind_function)
+        return button
+
     @staticmethod
-    def create_output_line(is_readonly = 0):
-        line=QLineEdit("")
+    def create_output_line(is_readonly=0):
+        line = QLineEdit("")
         line.setReadOnly(is_readonly)
         return line
 
+
 class Calculator:
     def __init__(self):
-        self.app=QApplication([])
-        self.window=QWidget()
-        self.output1=WidgetMaker.create_output_line()
-        self.output2=WidgetMaker.create_output_line(1)
-        self.buttons=
-        {
-            '1': WidgetMaker.create_button('1'),
-            '2': WidgetMaker.create_button('2'),
-            '3': WidgetMaker.create_button('3'),
-            '4': WidgetMaker.create_button('4'),
-            '5': WidgetMaker.create_button('5'),
-            '6': WidgetMaker.create_button('6'),
-            '7': WidgetMaker.create_button('7'),
-            '8': WidgetMaker.create_button('8'),
-            '9': WidgetMaker.create_button('9'),
-            '0': WidgetMaker.create_button('0'),
-            'sin': WidgetMaker.create_button('sin(x)')
-            'cos': WidgetMaker.create_button('cos(x)')
+        self.app = QApplication([])
+        self.window = QWidget()
+        self.layout = QGridLayout()
+        self.output1 = WidgetMaker.create_output_line()
+        self.output1_text = ""
+        self.output2 = WidgetMaker.create_output_line(1)
+        self.output2_text = ""
+        self.buttons = {}
+        self.set_buttons()
+        self.set_layout()
+
+    def button_clicked(self, text):
+        self.output1_text += text
+        self.output1.setText(self.output1_text)
+
+    def set_layout(self):
+        self.layout.addWidget(self.buttons['sin'], 1, 1)
+        self.layout.addWidget(self.buttons['cos'], 2, 1)
+        self.layout.addWidget(self.buttons['1'], 1, 2)
+        self.layout.addWidget(self.buttons['2'], 1, 3)
+        self.layout.addWidget(self.buttons['3'], 1, 4)
+        self.layout.addWidget(self.buttons['4'], 2, 2)
+        self.layout.addWidget(self.buttons['5'], 2, 3)
+        self.layout.addWidget(self.buttons['6'], 2, 4)
+        self.layout.addWidget(self.buttons['7'], 3, 2)
+        self.layout.addWidget(self.buttons['8'], 3, 3)
+        self.layout.addWidget(self.buttons['9'], 3, 4)
+        self.layout.addWidget(self.buttons['0'], 4, 3)
+        self.layout.addWidget(self.output1, 5, 5)
+        self.layout.addWidget(self.output2, 6, 5)
+
+    def set_buttons(self):
+        self.buttons ={
+            '1': WidgetMaker.create_button('1', lambda: self.button_clicked("1")),
+            '2': WidgetMaker.create_button('2', lambda: self.button_clicked("2")),
+            '3': WidgetMaker.create_button('3', lambda: self.button_clicked("3")),
+            '4': WidgetMaker.create_button('4', lambda: self.button_clicked("4")),
+            '5': WidgetMaker.create_button('5', lambda: self.button_clicked("5")),
+            '6': WidgetMaker.create_button('6', lambda: self.button_clicked("6")),
+            '7': WidgetMaker.create_button('7', lambda: self.button_clicked("7")),
+            '8': WidgetMaker.create_button('8', lambda: self.button_clicked("8")),
+            '9': WidgetMaker.create_button('9', lambda: self.button_clicked("9")),
+            '0': WidgetMaker.create_button('0', lambda: self.button_clicked("0")),
+            'sin': WidgetMaker.create_button('sin(x)', lambda: self.button_clicked("sin(x)")),
+            'cos': WidgetMaker.create_button('cos(x)', lambda: self.button_clicked("cos(x)")),
         }
 
-###############################################################
+    def run(self):
+        self.window.setLayout(self.layout)
+        self.window.show()
+        self.app.exec_()
 
-
-def on_button_clicked():
-    alert = QMessageBox()
-    alert.setText('You clicked the button!')
-    alert.exec_()
-
-
-def button_clicked(x):
-    l1.setText(x)
-    l2.setText(x)
-    l2.setReadOnly(1)
-
-
-##############################################################
-# app = QApplication([])
-# app.setStyleSheet("QPushButton { margin: 100px; }")
-# app.setStyleSheet("QPushButton { margin: 10ex; }")
-# app.setStyleSheet("QPushButton { margin-left: 20px; }")
-# app.setStyleSheet("QPushButton { margin-right: 40px; margin-left: 20px; }")
-# window = QWidget()
-bsin = QPushButton('sin(x)')
-bcos = QPushButton('cos(x)')
-Top = QPushButton('Top')
-Bottom = QPushButton('Bottom')
-bn1 = QPushButton('1')
-bn2 = QPushButton('2')
-bn3 = QPushButton('3')
-bn4 = QPushButton('4')
-bn5 = QPushButton('5')
-bn6 = QPushButton('6')
-bn7 = QPushButton('7')
-bn8 = QPushButton('8')
-bn9 = QPushButton('9')
-bn0 = QPushButton('0')
-l1 = QLineEdit("")
-l2 = QLineEdit("")
-bsin.clicked.connect(on_button_clicked)
-bcos.clicked.connect(on_button_clicked)
-Top.clicked.connect(lambda: button_clicked("TOP"))
-Bottom.clicked.connect(lambda: button_clicked("BOTTOM"))
-bn1.clicked.connect(lambda: button_clicked("1"))
-bn2.clicked.connect(lambda: button_clicked("2"))
-bn3.clicked.connect(lambda: button_clicked("3"))
-bn4.clicked.connect(lambda: button_clicked("4"))
-bn5.clicked.connect(lambda: button_clicked("5"))
-bn6.clicked.connect(lambda: button_clicked("6"))
-bn7.clicked.connect(lambda: button_clicked("7"))
-bn8.clicked.connect(lambda: button_clicked("8"))
-bn9.clicked.connect(lambda: button_clicked("9"))
-bn0.clicked.connect(lambda: button_clicked("0"))
-layout = QGridLayout()
-layout.addWidget(Top,1,0)
-layout.addWidget(Bottom,2,0)
-layout.addWidget(bsin,1,1)
-layout.addWidget(bcos,2,1)
-layout.addWidget(bn1,1,2)
-layout.addWidget(bn2,1,3)
-layout.addWidget(bn3,1,4)
-layout.addWidget(bn4,2,2)
-layout.addWidget(bn5,2,3)
-layout.addWidget(bn6,2,4)
-layout.addWidget(bn7,3,2)
-layout.addWidget(bn8,3,3)
-layout.addWidget(bn9,3,4)
-layout.addWidget(bn0,4,3)
-layout.addWidget(l1,5,5)
-layout.addWidget(l2,6,5)
-window.setLayout(layout)
-window.show()
-app.exec_()
+app=Calculator()
+app.run()
